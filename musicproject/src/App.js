92c9ea6ca3809dd4ec619e32b554ea98bb0c5e84
RogerRochoRibas <rogerrochoribas@gmail.com>
2022-05-16 21:32:34 +0200

    Made the Slider more Re-usable and improved it

--- musicproject/src/App.js
+++ musicproject/src/App.js
@@ -6,8 +6,8 @@ import { ChangeSongs } from "./ChangeSongs"
 
 function App() {
   const [musicList, setMusicList] = React.useState([]);
-  const [trackvalence, settrackvalence] = React.useState(500000)
-  const [trackarousal, settrackarousal] = React.useState(500000)
+  const [trackValence, setTrackValence] = React.useState(500000)
+  const [trackArousal, setTrackArousal] = React.useState(500000)
 
   //Needs 'Moesif Origin & CORS Changer' extension to work in production
   /* Ejemplos de parametros
@@ -25,14 +25,15 @@ function App() {
   resultsnumber=15
   resultsnumber=1
   */
+ // to get cover art: https://musicbrainz.org/doc/Cover_Art_Archive/API
+ // or https://developers.music-story.com/developers/artist
 
   function fetchMain() {
-    fetch(`http://musicovery.com/api/V6/playlist.php?&fct=getfrommood&popularitymax=100&popularitymin=50&starttrackid=&trackvalence=${trackvalence}&trackarousal=${trackarousal}&resultsnumber=15`)
+    fetch(`http://musicovery.com/api/V6/playlist.php?&fct=getfrommood&popularitymax=100&popularitymin=50&starttrackid=&trackvalence=${trackValence}&trackarousal=${trackArousal}&resultsnumber=15`)
       .then((response) => response.json())
       .then((music) => {
         setMusicList(music);
         console.log("music: ", music);
-        console.log("musicList: ", musicList);
       });
   }
   
@@ -43,7 +44,8 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome</h1>
-      <Slider settrackvalence={settrackvalence}></Slider>
+      <Slider setTrack={setTrackValence} identifier='Happy' identifierTwo='Sad'></Slider>
+      <Slider setTrack={setTrackArousal} identifier='Energetic' identifierTwo='Calm'></Slider>
       <ChangeSongs fetchMain={()=>fetchMain()}></ChangeSongs>
     </div>
   );
--- musicproject/src/Slider.js
+++ musicproject/src/Slider.js
@@ -1,20 +1,29 @@
 export function Slider(props) {
-    const output = document.querySelector('output')
+  const output = document.getElementById(props.identifier);
 
-const doThings = (events) => {
-	const { value, min, max, step, parentElement: parent } = events.target
-	const decimals = step && step.includes('.') ? step.split('.')[1] : 1
-	const percent = `${((value - min)/(max - min) * 100).toFixed(decimals)}%`
-	parent.style.setProperty('--p', percent)
-	output.value = `Happyness: ${percent}`
-    props.settrackvalence(percent*10000)
-}
-return <p><label class="heat-slider heat-slider--v">
-<span className="heat-slider--label">Are you happy?</span>
-<span className="heat-slider--input">
-    <input id="slider" type="range" min="0" max="100" onChange={(e)=>doThings(e)} />
-</span>
-</label>
+  const doThings = (events) => {
+    const { value, min, max, parentElement: parent } = events.target;
+    const percent = `${(((value - min) / (max - min)) * 100).toFixed(0)}%`;
+    parent.style.setProperty("--p", percent);
+    output.value = `${percent}`;
+    props.setTrack(value * 10000);
+  };
 
-<output>change the slider</output></p>
+  return (
+    <p>
+      <label class="heat-slider heat-slider--v">
+        <div className="heat-slider--label"><h3>{props.identifierTwo}</h3><h3>{props.identifier}</h3></div>
+        <span className="heat-slider--input">
+          <input
+            id="slider"
+            type="range"
+            min="0"
+            max="100"
+            onChange={(e) => doThings(e)}
+          />
+        </span>
+      </label>
+      <output id={props.identifier}></output>
+    </p>
+  );
 }
