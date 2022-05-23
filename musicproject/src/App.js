938aef7663ff56f9745aa03ade55149af6ab13c8
Miguel Cañate <rogerrochoribas@gmail.com>
2022-05-23 13:00:16 +0200

    Working on Genre Buttons

--- musicproject/src/App.js
+++ musicproject/src/App.js
@@ -8,11 +8,10 @@ import { TrackList } from "./components/TrackList";
 import { MainPanel } from "./components/MainPanel"
 
 function App() {
-  const [musicList, setMusicList] = React.useState([])
   const [trackList, setTrackList] = React.useState([])
   const [trackValence, setTrackValence] = React.useState(500000)
   const [trackArousal, setTrackArousal] = React.useState(500000)
-  const [genreNo, setGenreNo] = React.useState([])
+  const [genreNo, setGenreNo] = React.useState({})
 
   //Needs 'Moesif Origin & CORS Changer' extension to work in production
   /* Ejemplos de parametros
@@ -31,21 +30,19 @@ function App() {
   resultsnumber=1
   */
  // Genres ok: hiphop, electro, jazz, latin, rock, metal, pop, classical
- // Genres to consider: randb (r&B), soundtrack (cinema), folk, vocal pop (vocal)
- // Genres now showing up: funk, gospel, soul
+ // Genres to consider: randb (r&B), folk, vocal pop (vocal)
+ // Genres not showing up: funk, gospel, soul
  // to get cover art: https://musicbrainz.org/doc/Cover_Art_Archive/API
  // or https://developers.music-story.com/developers/artist
  // night mode toogle https://codepen.io/daviddarnes/pen/LdGYVG
 
   function fetchMain() {
-    fetch(`http://musicovery.com/api/V6/playlist.php?&fct=getfrommood&popularitymax=100&popularitymin=50&starttrackid=&trackvalence=${trackValence}&trackarousal=${trackArousal}&resultsnumber=15`)
+    fetch(`http://musicovery.com/api/V6/playlist.php?&fct=getfrommood&popularitymax=100&popularitymin=50&starttrackid=&trackvalence=${trackValence}&trackarousal=${trackArousal}&resultsnumber=15&genreNo=${genreNo}`)
       .then((response) => response.json())
       .then((music) => {
-        setMusicList(music);
         console.log("music: ", music);
         if (music.tracks) {
         setTrackList(music.tracks.track)
-        console.log('trackList: ',trackList)
         }
       });
   }
--- musicproject/src/components/GenreSelector.js
+++ musicproject/src/components/GenreSelector.js
@@ -6,20 +6,20 @@ export function GenreSelector(props) {
     <div id="genreGrid">
       <GenreButton genreNo={props.genreNo} setGenreNo={props.setGenreNo} genre="Rap" />
       <GenreButton genreNo={props.genreNo} setGenreNo={props.setGenreNo} genre="Electro" />
-      <GenreButton genreNo={props.genreNo} setGenreNo={props.setGenreNo} genre="Randb" />
+      <GenreButton genreNo={props.genreNo} setGenreNo={props.setGenreNo} genre="RandB" />
       <GenreButton genreNo={props.genreNo} setGenreNo={props.setGenreNo} genre="Latino" />
-      <GenreButton genreNo={props.genreNo} setGenreNo={props.setGenreNo} genre="Jazz" />
+      <GenreButton genreNo={props.genreNo} setGenreNo={props.setGenreNo} genre="Hip-Hop" />
       <GenreButton genreNo={props.genreNo} setGenreNo={props.setGenreNo} genre="Classical" />
-      <GenreButton genreNo={props.genreNo} setGenreNo={props.setGenreNo} genre="Gospel" />
+      <GenreButton genreNo={props.genreNo} setGenreNo={props.setGenreNo} genre="Jazz" />
       <GenreButton genreNo={props.genreNo} setGenreNo={props.setGenreNo} genre="Cinema" />
       <GenreButton genreNo={props.genreNo} setGenreNo={props.setGenreNo} genre="Blues" />
       <GenreButton genreNo={props.genreNo} setGenreNo={props.setGenreNo} genre="World" />
       <GenreButton genreNo={props.genreNo} setGenreNo={props.setGenreNo} genre="Metal" />
       <GenreButton genreNo={props.genreNo} setGenreNo={props.setGenreNo} genre="Reggae" />
       <GenreButton genreNo={props.genreNo} setGenreNo={props.setGenreNo} genre="Rock" />
-      <GenreButton genreNo={props.genreNo} setGenreNo={props.setGenreNo} genre="Soul" />
+      <GenreButton genreNo={props.genreNo} setGenreNo={props.setGenreNo} genre="Folk" />
       <GenreButton genreNo={props.genreNo} setGenreNo={props.setGenreNo} genre="Vocal" />
-      <GenreButton genreNo={props.genreNo} setGenreNo={props.setGenreNo} genre="Funk" />
+      <GenreButton genreNo={props.genreNo} setGenreNo={props.setGenreNo} genre="Country" />
       <GenreButton genreNo={props.genreNo} setGenreNo={props.setGenreNo} genre="Pop" />
       <GenreButton genreNo={props.genreNo} setGenreNo={props.setGenreNo} genre="Disco" />
     </div>
@@ -27,18 +27,20 @@ export function GenreSelector(props) {
 }
 
 function GenreButton(props) {
-  async function changeGenre(genre,value) {
-    try { let newGenreNo = await {...props.genreNo};
-    console.log(newGenreNo)
-    newGenreNo[genre]= await value;
+  function changeGenre(genre,value) {
+    console.log('value',genre)
+    console.log('value',value)
+    /* try {
+    let newGenreNo = {...props.genreNo};
+    newGenreNo.indexOf(genre) === -1 ? newGenreNo.push(genre) : newGenreNo.splice(newGenreNo.indexOf(genre), 1);
     props.setGenreNo(newGenreNo);
   } catch(err)
-  {console.log(err)}
+  {console.log(err)} */
   }
   return (
       <article className={props.genre}>
         <div className="genreBox">
-          <input type="checkbox" onChange={e => changeGenre(props.genre,e.target.value)} />
+          <input type="checkbox" onClick={e => changeGenre(props.genre,this.checked)} />
           <div>
             <span className="genre">{props.genre}</span>
           </div>
