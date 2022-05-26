9c7c6305e244694fdf1208bc1c8255ee77c3087f
Miguel Cañate <rogerrochoribas@gmail.com>
2022-05-26 12:57:24 +0200

    Tracklist CSS

--- musicproject/src/App.js
+++ musicproject/src/App.js
@@ -1,16 +1,36 @@
 import "./css/Slider.css";
 import "./css/Button.css";
-import "./css/TrackList.css"
-import "./css/MainPanel.css"
+import "./css/TrackList.css";
+import "./css/MainPanel.css";
 import React from "react";
 import { TrackList } from "./components/TrackList";
-import { MainPanel } from "./components/MainPanel"
+import { MainPanel } from "./components/MainPanel";
+import { dataExample } from "./dataExample";
 
 function App() {
-  const [trackList, setTrackList] = React.useState([])
-  const [trackValence, setTrackValence] = React.useState(500000)
-  const [trackArousal, setTrackArousal] = React.useState(500000)
-  const [genreNo, setGenreNo] = React.useState({'Rap':false,'Electro':false,'HipHop':false,'Soul':false,'Jazz':false,'Classical':false,'Blues':false,'Cinema':false,'European':false,'World':false,'Metal':false,'Reggae':false,'Rock':false,'Folk':false,'Vocal':false,'Country':false,'Pop':false,'Funk':false})
+  const [trackList, setTrackList] = React.useState([]);
+  const [trackValence, setTrackValence] = React.useState(500000);
+  const [trackArousal, setTrackArousal] = React.useState(500000);
+  const [genreNo, setGenreNo] = React.useState({
+    Rap: false,
+    Electro: false,
+    HipHop: false,
+    Soul: false,
+    Jazz: false,
+    Classical: false,
+    Blues: false,
+    Cinema: false,
+    European: false,
+    World: false,
+    Metal: false,
+    Reggae: false,
+    Rock: false,
+    Folk: false,
+    Vocal: false,
+    Country: false,
+    Pop: false,
+    Funk: false,
+  });
 
   //Needs 'Moesif Origin & CORS Changer' extension to work in production
   /* Ejemplos de parametros
@@ -28,75 +48,128 @@ function App() {
   resultsnumber=15
   resultsnumber=1
   */
- /* Genre categories
- 1. Rap:gangsta rap,rap r&b,
- 2. Electro: big beat,breakbeat,broken-beat,dance electro,dj,downbeat,ambient,drum-and-bass,dubstep,electro,electro ambient,electro atmospheric,electro latin,electro minimal,electroclash,electronica,eurodance,house,jungle,techno,trance,
- 3. European:French 00's,French 60's,French 70's,French 80's,French 90's,French music,German music,German pop,German pop 00's,German pop 60's,German pop 70's,German pop 80's,German pop 90's,German vocal,Italian music,Italian pop,Italian pop 00's,Italian pop 90's,Italian pop 80's,Italian pop 70's,Italian pop 60's,New French pop,New German pop,Norwegian music,Norwegian pop,Portuguese music,Scandinavian music,Spanish music,Spanish pop 00's,Spanish pop 60's,Spanish pop 70's,Spanish pop 80's,Spanish pop 90's,Spanish rock,Spanish vocal,Swedish music,Swedish pop,chanson française,chanson française 00s,chanson française 60s,French pop 60's,chanson française 70s,chanson française 80s,French pop 80's,chanson française 90s,Deutche Musik,deutsche pop,deutsche pop 00's,deutsche pop 60's,deutsche pop 70's,deutsche pop 80's,deutsche pop 90's,German pop,schlager,musica española,musica italiana,musique française,Neue Deutsche Welle,norsk musikk,norsk pop,Nouvelle scène,pop español,pop español 00's,pop español 60's,pop español 70's,pop español 80's,pop español 90's,pop italiano,pop italiano 00's,pop italiano 60's,pop italiano 70's,pop italiano 80's,pop italiano 90's,Rock en Espanol,rock français,Schlager,sous la douche,svensk musik,svensk pop,variété française,variété française 00s,variété française 60s,variété française 70s,variété française 80s,variété française 90s,
- 4. Latino: bachata,mambo,mpb,reggaeton,salsa,samba,latino,
- 5. Hip-hop:dirty-south,hip-hop,trap,trip-hop,
- 6. Classical: big-band,classical,classical ballet,classical baroque,classical cello,classical chamber,classical classicism,classical concerto,classical contemporary,classical guitar,classical harp,classical lied,classical modern,classical opera,classical piano,classical reinassance,classical romantic,classical sacred,classical symphonic,classical trumpet,classical violon,dancehall,modern-classical,clásica,classical,classica,classique,classique ballet,classique baroque,classique chambre,classique concertos,classique contemporain,classique moderne,classique opera,classique piano,classique romantique,classique sacré,classique symphonique,classique violon,classique violoncelle,classique vocal,danser tempo assez lent,dîner,dîner romantique,Klassik,Klassisk,
- 7. Jazz: jazz,acid-jazz,bebop,blue-eyed-soul,chicago-soul,dixieland,free jazz,funk,fusion jazz,hard bop,jazz electro,jazz latin,jazz piano,jazz rock,jazz sax,jazzy,lounge,manouche,motown,quiet storm,r&b,swing,
- 8. Cinema:cinema action,cinema adventure,cinema animation,cinema biopic,cinema bollywood,cinema comedy,cinema drama,cinema fantasy,cinema history,cinema horror,cinema martial art,cinema musical,cinema nature,cinema peplum,cinema romance,cinema science fiction,cinema spy,cinema thriller,cinema tragicomedy,cinema war, cinema western,soundtrack,cinéma comédie dramatique,cinema tragicomedy,cinéma dessin animé,cinema animation,cinéma dramatique,cinema drama,cinéma espionnage,cinéma spy,cinéma guerre,cinema war,cinéma horreur,cinema horror,
- 9. Blues: blues,acoustic blues,delta blues,blues jazz,blues roots,blues soul,chicago-blues,doo-wop,electric blues,soul,southern-soul,
- 10. World: african,afrobeat,americana,arabic,australian,bossa-nova,Brazilian,Canadian,chanson,Cuban music,dance latin,ethnic,fado,fusion world,gospel,latin,pagode,tropicalia,world,
- 11. Metal: black metal,death metal,death core,doom metal,heavy metal,metal,nu metal,screamo,sludge,speed metal,thrash,
- 12. Reggae: baile funk,dub,ragga,rasta,reggae,rocksteady,ska,ska-punk,
- 13. Rock:blues rock,darkwave,emo,French rock,fusion rock,glam,gothic,grunge,guitar hero,hard-rock,indie rock,kraut rock,new-wave,psychedelic,punk,rock,rock'n'roll,rockabilly,showgaxe,
- 14. Folk:brazilian folk,folk,folklore,celtic,flamenco,gypsy,tango,
- 15. Vocal:classical vocal,vocal jazz, vocal pop,
- 16. Country:bluegrass,blues country,country,sertanejo,
- 17. Pop: baroque pop, boys-band,brit-pop,bubblegum,british-invasion,cantautori,euro pop,eurovision,French pop,girlsband,indie pop,orchestral pop,pop,pop latin,power pop,synthpop,teen,new-age,reading news,
- 18. Disco: disco,garage,*/
-
- // Categorias vacias: Disco, Latino, Europea
- // to get cover art: https://musicbrainz.org/doc/Cover_Art_Archive/API
- // or https://developers.music-story.com/developers/artist
- // night mode toogle https://codepen.io/daviddarnes/pen/LdGYVG
+  // to get cover art: https://musicbrainz.org/doc/Cover_Art_Archive/API
+  // or https://developers.music-story.com/developers/artist
+  // night mode toogle https://codepen.io/daviddarnes/pen/LdGYVG
 
   function fetchMain() {
-    let Rap = '';let Electro = '';let HipHop = '';let Soul = '';let Jazz = ''; let Classical = '';let Blues = '';let Cinema = '';let European = '';
-    let World = '';let Metal = '';let Reggae = '';let Rock = '';let Folk = '';let Vocal = '';let Country = '';let Pop = '';let Funk = '';
-    if(genreNo.Rap) {Rap = 'gangsta rap,rap,rap randb,'};
-    if(genreNo.Electro) {Electro = 'big beat,breakbeat,broken-beat,dance electro,dj,downbeat,ambient,drum-and-bass,dubstep,electro,electro ambient,electro atmospheric,electro latin,electro minimal,electroclash,electronica,eurodance,house,jungle,techno,trance,'};
-    if(genreNo.HipHop) {HipHop = 'dirty-south,hip hop,trap,trip-hop,'};
-    if(genreNo.Soul) {Soul = 'blue-eyed-soul,chicago-soul,blues soul,soul,southern-soul,'};
-    if(genreNo.Jazz) {Jazz = 'jazz,acid-jazz,bebop,dixieland,free jazz,fusion jazz,hard bop,jazz electro,jazz latin,jazz piano,jazz rock,jazz sax,jazzy,lounge,manouche,motown,quiet storm,swing,randb,'};
-    if(genreNo.Classical) {Classical = 'big-band,classical,classical ballet,classical baroque,classical cello,classical chamber,classical classicism,classical concerto,classical contemporary,classical guitar,classical harp,classical lied,classical modern,classical opera,classical piano,classical reinassance,classical romantic,classical sacred,classical symphonic,classical trumpet,classical violon,dancehall,modern-classical,clásica,classical,classica,classique,classique ballet,classique baroque,classique chambre,classique concertos,classique contemporain,classique moderne,classique opera,classique piano,classique romantique,classique sacré,classique symphonique,classique violon,classique violoncelle,classique vocal,danser tempo assez lent,dîner,dîner romantique,Klassik,Klassisk,'};
-    if(genreNo.Blues) {Blues = 'blues,acoustic blues,delta blues,blues jazz,blues roots,chicago-blues,doo-wop,electric blues,'};
-    if(genreNo.Cinema) {Cinema = 'cinema action,cinema adventure,cinema animation,cinema biopic,cinema bollywood,cinema comedy,cinema drama,cinema fantasy,cinema history,cinema horror,cinema martial art,cinema musical,cinema nature,cinema peplum,cinema romance,cinema science fiction,cinema spy,cinema thriller,cinema tragicomedy,cinema war,cinema western,soundtrack,cinéma comédie dramatique,cinema tragicomedy,cinéma dessin animé,cinema animation,cinéma dramatique,cinema drama,cinéma espionnage,cinéma spy,cinéma guerre,cinema war,cinéma horreur,cinema horror,'};
-    if(genreNo.European) {European = "flamenco,chanson,gospel,latin,French 00's,French 60's,French 70's,French 80's,French 90's,French music,German music,German pop,German pop 00's,German pop 60's,German pop 70's,German pop 80's,German pop 90's,German vocal,Italian music,Italian pop,Italian pop 00's,Italian pop 90's,Italian pop 80's,Italian pop 70's,Italian pop 60's,New French pop,New German pop,Norwegian music,Norwegian pop,Portuguese music,Scandinavian music,Spanish music,Spanish pop 00's,Spanish pop 60's,Spanish pop 70's,Spanish pop 80's,Spanish pop 90's,Spanish rock,Spanish vocal,Swedish music,Swedish pop,chanson française,chanson française 00s,chanson française 60s,French pop 60's,chanson française 70s,chanson française 80s,French pop 80's,chanson française 90s,Deutche Musik,deutsche pop,deutsche pop 00's,deutsche pop 60's,deutsche pop 70's,deutsche pop 80's,deutsche pop 90's,German pop,schlager,musica española,musica italiana,musique française,Neue Deutsche Welle,norsk musikk,norsk pop,Nouvelle scène,pop español,pop español 00's,pop español 60's,pop español 70's,pop español 80's,pop español 90's,pop italiano,pop italiano 00's,pop italiano 60's,pop italiano 70's,pop italiano 80's,pop italiano 90's,Rock en Espanol,rock français,Schlager,sous la douche,svensk musik,svensk pop,variété française,variété française 00s,variété française 60s,variété française 70s,variété française 80s,variété française 90s,"};
-    if(genreNo.World) {World = 'african,afrobeat,americana,arabic,australian,bossa-nova,Brazilian,Canadian,Cuban music,dance latin,ethnic,fado,fusion world,pagode,tropicalia,world,'};
-    if(genreNo.Metal) {Metal = 'black metal,death metal,death core,doom metal,heavy metal,metal,nu metal,screamo,sludge,speed metal,thrash,'};
-    if(genreNo.Reggae) {Reggae = 'dub,ragga,rasta,reggae,rocksteady,ska,ska-punk,'};
-    if(genreNo.Rock) {Rock = "blues rock,darkwave,emo,French rock,fusion rock,glam,gothic,grunge,guitar hero,hard-rock,indie rock,kraut rock,new-wave,psychedelic,punk,rock,rock'n'roll,rockabilly,showgaxe,"};
-    if(genreNo.Folk) {Folk = 'brazilian folk,folk,folklore,celtic,gypsy,tango,'};
-    if(genreNo.Vocal) {Vocal = 'classical vocal,vocal jazz,vocal pop,'};
-    if(genreNo.Country) {Country = 'bluegrass,blues country,country,sertanejo,'};
-    if(genreNo.Pop) {Pop = 'baroque pop,boys-band,brit-pop,bubblegum,british-invasion,cantautori,euro pop,eurovision,French pop,girlsband,indie pop,orchestral pop,pop,pop latin,power pop,synthpop,teen,new-age,reading news,'};
-    if(genreNo.Funk) {Funk = 'baile funk,funk,'};
-    let proxy = 
-    fetch(`https://musicovery.com/api/V6/playlist.php?&fct=getfrommood&popularitymax=100&popularitymin=50&starttrackid=&trackvalence=${trackValence}&trackarousal=${trackArousal}&resultsnumber=15&genreNo=${Rap}${Electro}${HipHop}${Soul}${Metal}${World}${European}${Cinema}${Funk}${Classical}${Reggae}${Rock}${Vocal}${Pop}${Country}${Folk}${Jazz}${Blues}`)
-      .then((response) => response.json())
+    let Rap = "";
+    let Electro = "";
+    let HipHop = "";
+    let Soul = "";
+    let Jazz = "";
+    let Classical = "";
+    let Blues = "";
+    let Cinema = "";
+    let European = "";
+    let World = "";
+    let Metal = "";
+    let Reggae = "";
+    let Rock = "";
+    let Folk = "";
+    let Vocal = "";
+    let Country = "";
+    let Pop = "";
+    let Funk = "";
+    if (genreNo.Rap) {
+      Rap = "gangsta rap,rap,rap randb,";
+    }
+    if (genreNo.Electro) {
+      Electro =
+        "big beat,breakbeat,broken-beat,dance electro,dj,downbeat,ambient,drum-and-bass,dubstep,electro,electro ambient,electro atmospheric,electro latin,electro minimal,electroclash,electronica,eurodance,house,jungle,techno,trance,";
+    }
+    if (genreNo.HipHop) {
+      HipHop = "dirty-south,hip hop,trap,trip-hop,";
+    }
+    if (genreNo.Soul) {
+      Soul = "blue-eyed-soul,chicago-soul,blues soul,soul,southern-soul,";
+    }
+    if (genreNo.Jazz) {
+      Jazz =
+        "jazz,acid-jazz,bebop,dixieland,free jazz,fusion jazz,hard bop,jazz electro,jazz latin,jazz piano,jazz rock,jazz sax,jazzy,lounge,manouche,motown,quiet storm,swing,randb,";
+    }
+    if (genreNo.Classical) {
+      Classical =
+        "big-band,classical,classical ballet,classical baroque,classical cello,classical chamber,classical classicism,classical concerto,classical contemporary,classical guitar,classical harp,classical lied,classical modern,classical opera,classical piano,classical reinassance,classical romantic,classical sacred,classical symphonic,classical trumpet,classical violon,dancehall,modern-classical,clásica,classical,classica,classique,classique ballet,classique baroque,classique chambre,classique concertos,classique contemporain,classique moderne,classique opera,classique piano,classique romantique,classique sacré,classique symphonique,classique violon,classique violoncelle,classique vocal,danser tempo assez lent,dîner,dîner romantique,Klassik,Klassisk,";
+    }
+    if (genreNo.Blues) {
+      Blues =
+        "blues,acoustic blues,delta blues,blues jazz,blues roots,chicago-blues,doo-wop,electric blues,";
+    }
+    if (genreNo.Cinema) {
+      Cinema =
+        "cinema action,cinema adventure,cinema animation,cinema biopic,cinema bollywood,cinema comedy,cinema drama,cinema fantasy,cinema history,cinema horror,cinema martial art,cinema musical,cinema nature,cinema peplum,cinema romance,cinema science fiction,cinema spy,cinema thriller,cinema tragicomedy,cinema war,cinema western,soundtrack,cinéma comédie dramatique,cinema tragicomedy,cinéma dessin animé,cinema animation,cinéma dramatique,cinema drama,cinéma espionnage,cinéma spy,cinéma guerre,cinema war,cinéma horreur,cinema horror,";
+    }
+    if (genreNo.European) {
+      European =
+        "flamenco,chanson,gospel,latin,French 00's,French 60's,French 70's,French 80's,French 90's,French music,German music,German pop,German pop 00's,German pop 60's,German pop 70's,German pop 80's,German pop 90's,German vocal,Italian music,Italian pop,Italian pop 00's,Italian pop 90's,Italian pop 80's,Italian pop 70's,Italian pop 60's,New French pop,New German pop,Norwegian music,Norwegian pop,Portuguese music,Scandinavian music,Spanish music,Spanish pop 00's,Spanish pop 60's,Spanish pop 70's,Spanish pop 80's,Spanish pop 90's,Spanish rock,Spanish vocal,Swedish music,Swedish pop,chanson française,chanson française 00s,chanson française 60s,French pop 60's,chanson française 70s,chanson française 80s,French pop 80's,chanson française 90s,Deutche Musik,deutsche pop,deutsche pop 00's,deutsche pop 60's,deutsche pop 70's,deutsche pop 80's,deutsche pop 90's,German pop,schlager,musica española,musica italiana,musique française,Neue Deutsche Welle,norsk musikk,norsk pop,Nouvelle scène,pop español,pop español 00's,pop español 60's,pop español 70's,pop español 80's,pop español 90's,pop italiano,pop italiano 00's,pop italiano 60's,pop italiano 70's,pop italiano 80's,pop italiano 90's,Rock en Espanol,rock français,Schlager,sous la douche,svensk musik,svensk pop,variété française,variété française 00s,variété française 60s,variété française 70s,variété française 80s,variété française 90s,";
+    }
+    if (genreNo.World) {
+      World =
+        "african,afrobeat,americana,arabic,australian,bossa-nova,Brazilian,Canadian,Cuban music,dance latin,ethnic,fado,fusion world,pagode,tropicalia,world,";
+    }
+    if (genreNo.Metal) {
+      Metal =
+        "black metal,death metal,death core,doom metal,heavy metal,metal,nu metal,screamo,sludge,speed metal,thrash,";
+    }
+    if (genreNo.Reggae) {
+      Reggae = "dub,ragga,rasta,reggae,rocksteady,ska,ska-punk,";
+    }
+    if (genreNo.Rock) {
+      Rock =
+        "blues rock,darkwave,emo,French rock,fusion rock,glam,gothic,grunge,guitar hero,hard-rock,indie rock,kraut rock,new-wave,psychedelic,punk,rock,rock'n'roll,rockabilly,showgaxe,";
+    }
+    if (genreNo.Folk) {
+      Folk = "brazilian folk,folk,folklore,celtic,gypsy,tango,";
+    }
+    if (genreNo.Vocal) {
+      Vocal = "classical vocal,vocal jazz,vocal pop,";
+    }
+    if (genreNo.Country) {
+      Country = "bluegrass,blues country,country,sertanejo,";
+    }
+    if (genreNo.Pop) {
+      Pop =
+        "baroque pop,boys-band,brit-pop,bubblegum,british-invasion,cantautori,euro pop,eurovision,French pop,girlsband,indie pop,orchestral pop,pop,pop latin,power pop,synthpop,teen,new-age,reading news,";
+    }
+    if (genreNo.Funk) {
+      Funk = "baile funk,funk,";
+    }
+
+    fetch(
+      `https://musicovery.com/api/V6/playlist.php?&fct=getfrommood&popularitymax=100&popularitymin=50&starttrackid=&trackvalence=${trackValence}&trackarousal=${trackArousal}&resultsnumber=15&genreNo=${Rap}${Electro}${HipHop}${Soul}${Metal}${World}${European}${Cinema}${Funk}${Classical}${Reggae}${Rock}${Vocal}${Pop}${Country}${Folk}${Jazz}${Blues}`
+    )
+      /*.then((response) => response.json())
       .then((music) => {
         console.log("music request: ", music.request);
         console.log("music tracks: ", music);
         if (music.tracks) {
-        setTrackList(music.tracks.track)
-        } else setTrackList([])
-      });
+          setTrackList(music.tracks.track);
+        } else*/ setTrackList(dataExample);
+      /* });*/
   }
-  
+ 
   React.useEffect(() => {
     fetchMain();
   }, []);
 
   return (
     <div className="App">
-      <MainPanel genreNo={genreNo} setGenreNo={setGenreNo} setTrackValence={setTrackValence} setTrackArousal={setTrackArousal} fetchMain={() => fetchMain()}/>
-      <div className='Tracks'><TrackList trackList={trackList}/></div>
+      <MainPanel
+        genreNo={genreNo}
+        setGenreNo={setGenreNo}
+        setTrackValence={setTrackValence}
+        setTrackArousal={setTrackArousal}
+        fetchMain={() => fetchMain()}
+      />
+      <div className="trackContainer">
+        <ul className="Tracks">
+          <TrackList trackList={trackList} />
+        </ul>
+      </div>
     </div>
   );
 }
 
-export const ContextProvider = React.createContext()
+export const ContextProvider = React.createContext();
 export default App;
--- musicproject/src/components/TrackList.js
+++ musicproject/src/components/TrackList.js
@@ -3,11 +3,13 @@ export function TrackList(props) {
   if (props.trackList.length > 0) { 
     let SongsMounted = props.trackList.map((element, index) => {
       return (
-          <div className="track">
-              <td className="track_title">{element.title}</td>
-              <td className="track_artist"> - {element.artist_display_name}</td>
-              <td className="track_genre">{element.genre}</td>
-          </div>
+          <li className={`track ${element.genre}`}>
+            <h2>{element.genre}</h2>
+            <div className="centerTrack">
+              <h3 className="track_title">{element.title}</h3>
+              <p className="track_artist">{element.artist_display_name}</p>
+              <button>Read more</button></div>
+          </li>
       );
     });
     return SongsMounted;
