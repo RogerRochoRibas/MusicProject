ed71f91c9406c78c2ae5734792fd5f6b6bebf6f3
RogerRochoRibas <rogerrochoribas@gmail.com>
2022-10-25 16:13:43 +0200

    change path to use in github pages

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
     <Router className="App">
       <Routes>
         <Route
-          path="/"
+          path="/MusicProject"
           element={
             <>
               <MainPanel
